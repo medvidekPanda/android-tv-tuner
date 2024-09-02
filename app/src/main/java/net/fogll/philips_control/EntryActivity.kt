bc2d25f86a6b60dff99f48fc7105275f498cd466
Jan Ptacek <honza@fogll.net>
2024-09-02 10:30:40 +0200

    Add log to start service

--- app/src/main/java/net/fogll/philips_control/EntryActivity.kt
+++ app/src/main/java/net/fogll/philips_control/EntryActivity.kt
@@ -1,57 +1,41 @@
 package net.fogll.philips_control
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.database.Cursor
-import android.media.tv.TvContract
-import android.media.tv.TvInputInfo
 import android.media.tv.TvInputManager
-import android.media.tv.TvInputService
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class EntryActivity : AppCompatActivity() {
-
-    //private val permissionReadListings = "com.android.providers.tv.permission.READ_TV_LISTINGS"
-    private val permissionReadListings = "android.permission.ACCESS_COARSE_LOCATION"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("TVInput", "------------onCreate-------------")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry)
 
         val tvInputManager = this.getSystemService(Context.TV_INPUT_SERVICE) as TvInputManager
-        val tvInput =
-            tvInputManager.tvInputList.find { it.id.contains("HW0") }
+        val tvInput = tvInputManager.tvInputList.find { it.id.contains("HW0") }
 
         Log.d(
-            "TVInput",
-            "TV Input Manager: ${tvInput?.id} - ${tvInput?.serviceInfo} - ${
+            "TVInput", "TV Input Manager: ${tvInput?.id} - ${tvInput?.serviceInfo} - ${
                 tvInput?.loadLabel(
                     this
                 )
             }"
         )
 
+        val intent = Intent(this, MyTvInputService::class.java)
+        startService(intent)
+
         if (tvInput?.id == null) {
             Log.d("TVInput", "HW input not found")
             return;
         }
 
-        val intent = Intent(this, MyTvInputService::class.java)
-        Log.d("TVInput", "Starting TV input service")
-
-        val channelUri = Uri.parse("content://net.fogll.tvtunertest/channel/1")
-        val tvInputService = MyTvInputService()
-        val session = tvInputService.onCreateSession(tvInput.id)
+        //val channelUri = Uri.parse("content://net.fogll.philips_control/channel/1")
+        //val tvInputService = MyTvInputService()
+        //val session = tvInputService.onCreateSession(tvInput.id)
         //session.onTune(channelUri)
 
 
--- app/src/main/java/net/fogll/philips_control/MyTvInputService.kt
+++ app/src/main/java/net/fogll/philips_control/MyTvInputService.kt
@@ -1,6 +1,8 @@
 package net.fogll.philips_control
 
+import android.content.Intent
 import android.media.tv.TvInputService
+import android.os.IBinder
 import android.util.Log
 
 
@@ -13,6 +15,12 @@ class MyTvInputService : TvInputService() {
     override fun onCreateSession(inputId: String): Session {
         Log.d("TVInput", "Creating session for input: $inputId")
         return MySession(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("TVInput", "MyTvInputService started")
+        // Your service logic here
+        return START_STICKY
     }
 
     fun onReleaseSession(session: Session) {
